e52c7561f9e7c7ba663087c127133090b04ddba3
Luke Winikates <luke.winikates@gmail.com>
2016-01-06 11:47:58 -0800

    require React in boot.js; it doesn't seem to work in the .NET server otherwise

--- dotnet-toolbox.ui/src/js/boot.js
+++ dotnet-toolbox.ui/src/js/boot.js
@@ -1,3 +1,4 @@
 var ReactDOM = require('react/lib/ReactDOM');
+var React = require('react');
 var PackageAdder = require('./components/package-adder');
-ReactDOM.render(<PackageAdder />, document.getElementById('root'));
+ReactDOM.render(React.createElement(PackageAdder, null), document.getElementById('root'));

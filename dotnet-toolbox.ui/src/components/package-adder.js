90510f8bd1a1f82d0d23ada5e639768bba5e8156
Luke Winikates <luke.winikates@gmail.com>
2016-01-06 11:02:59 -0800

    add styled package-adder prompt
      - copy bootstrap's root scss file with most dependencies commented out, so they can be selectively added in in the right order as needed
      - pull local components into their own subfiles

--- dotnet-toolbox.ui/src/components/package-adder.js
+++ dotnet-toolbox.ui/src/components/package-adder.js
@@ -4,10 +4,19 @@ var ReactDOM = require('react/lib/ReactDOM');
 var PackageAdder = React.createClass({
   render() {
     return (
-      <div>
-        <button className="btn btn-default">+</button>
+      <div className="container up-xxxl panel paxxxl bg-neutral-11 panel-shadow">
+        <form className="form-inline">
+          <div className="h3">
+            Help others by sharing Nuget packages that have helped you
+          </div>
+          <div className="form-group">
+            <label className="sr-only" htmlFor="package-name">Package Name</label>
+            <input className="form-control" id="package-name" placeholder="Nuget Package Name" type="text"/>
+          </div>
+          <button className="btn btn-default" type="submit">add +</button>
+        </form>
       </div>
-    );
+  );
   }
 });
 
